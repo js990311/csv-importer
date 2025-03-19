12bb3cc2130f65910f3cf6c7f7cab73c8be34996
Lee jong sik <102013392+js990311@users.noreply.github.com>
2025-03-19 21:32:35 +0900

    feat(gradle): add task: loadCsv

--- src/main/java/com/csv/importer/ImporterApplication.java
+++ src/main/java/com/csv/importer/ImporterApplication.java
@@ -1,8 +1,12 @@
 package com.csv.importer;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan(basePackages = "com.rejs.csvloader")
+@Slf4j
 @SpringBootApplication
 public class ImporterApplication {
 
