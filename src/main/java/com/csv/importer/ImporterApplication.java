2a58231a6bb6b0290dfcd32ecc71763c7eee9080
Lee jong sik <102013392+js990311@users.noreply.github.com>
2025-03-21 17:31:55 +0900

    feat: CsvLoadConfiguration

--- src/main/java/com/csv/importer/ImporterApplication.java
+++ src/main/java/com/csv/importer/ImporterApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan(basePackages = "com.rejs.csvloader")
 @Slf4j
 @SpringBootApplication
 public class ImporterApplication {
---
+++ src/main/java/com/csv/importer/config/CsvLoadConfiguration.java
@@ -0,0 +1,32 @@
+package com.csv.importer.config;
+
+import com.rejs.csvloader.CsvLoadCommandLineRunner;
+import com.rejs.csvloader.config.AbstractCsvLoadConfiguration;
+import com.rejs.csvloader.config.configurer.CsvColumnValidateServiceConfigurer;
+import com.rejs.csvloader.config.configurer.CsvLoadBuilder;
+import com.rejs.csvloader.config.configurer.JdbcBatchInsertRepositoryConfigurer;
+import com.rejs.csvloader.file.LocalFileSystemAccessObject;
+import com.rejs.csvloader.validator.impl.NotBlankValidator;
+import com.rejs.csvloader.yaml.ImportPropertiesLoader;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+public class CsvLoadConfiguration extends AbstractCsvLoadConfiguration {
+
+    @Override
+    public CsvLoadBuilder csvLoadBuilder(CsvLoadBuilder builder) {
+        return builder
+                .jdbcBatchInsertRepository((new JdbcBatchInsertRepositoryConfigurer()).withDefault())
+                .csvColumnValidateService((new CsvColumnValidateServiceConfigurer()).withDefault()
+                        .custom(
+                                new NotBlankValidator()
+                        ));
+    }
+
+    @Bean
+    public CsvLoadCommandLineRunner csvLoadCommandLineRunner(){
+        return new CsvLoadCommandLineRunner(csvLoadService(), new ImportPropertiesLoader(new LocalFileSystemAccessObject()));
+    }
+}
