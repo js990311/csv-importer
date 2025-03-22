a103dc9ef6f6b3ccfe242584802e7fe8a9cee3fb
Lee jong sik <102013392+js990311@users.noreply.github.com>
2025-03-22 16:14:57 +0900

    chore: csvloader 0.1.3

--- src/main/java/com/csv/importer/config/CsvLoadConfiguration.java
+++ src/main/java/com/csv/importer/config/CsvLoadConfiguration.java
@@ -1,33 +1,21 @@
 package com.csv.importer.config;
 
-import com.rejs.csvloader.CsvLoadCommandLineRunner;
-import com.rejs.csvloader.config.AbstractCsvLoadConfiguration;
+import com.rejs.csvloader.config.AbstractCsvLoadCommandLineRunnerConfiguration;
 import com.rejs.csvloader.config.configurer.CsvColumnValidateServiceConfigurer;
 import com.rejs.csvloader.config.configurer.CsvLoadBuilder;
 import com.rejs.csvloader.config.configurer.JdbcBatchInsertRepositoryConfigurer;
-import com.rejs.csvloader.file.LocalFileSystemAccessObject;
-import com.rejs.csvloader.validator.impl.NotBlankValidator;
-import com.rejs.csvloader.yaml.ImportPropertiesLoader;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Profile("loadcsv")
 @Configuration
-public class CsvLoadConfiguration extends AbstractCsvLoadConfiguration {
+public class CsvLoadConfiguration extends AbstractCsvLoadCommandLineRunnerConfiguration {
 
     @Override
     public CsvLoadBuilder csvLoadBuilder(CsvLoadBuilder builder) {
         return builder
                 .jdbcBatchInsertRepository((new JdbcBatchInsertRepositoryConfigurer()).withDefault())
-                .csvColumnValidateService((new CsvColumnValidateServiceConfigurer()).withDefault()
-                        .custom(
-                                new NotBlankValidator()
-                        ));
-    }
-
-    @Bean
-    public CsvLoadCommandLineRunner csvLoadCommandLineRunner(){
-        return new CsvLoadCommandLineRunner(csvLoadService(), new ImportPropertiesLoader(new LocalFileSystemAccessObject()));
+                .csvColumnValidateService((new CsvColumnValidateServiceConfigurer()).withDefault())
+        ;
     }
 }
