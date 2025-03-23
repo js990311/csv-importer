139758748c9f7928de89eb5a7cc7c32508afc392
Lee jong sik <102013392+js990311@users.noreply.github.com>
2025-03-23 13:54:28 +0900

    feat: MultipartFileLocalSystemAccessObject

--- src/main/java/com/csv/importer/config/CsvLoadConfiguration.java
+++ src/main/java/com/csv/importer/config/CsvLoadConfiguration.java
@@ -1,9 +1,11 @@
 package com.csv.importer.config;
 
+import com.csv.importer.domain.file.service.MultipartFileLocalSystemAccessObject;
 import com.rejs.csvloader.config.AbstractCsvLoadCommandLineRunnerConfiguration;
 import com.rejs.csvloader.config.configurer.CsvColumnValidateServiceConfigurer;
 import com.rejs.csvloader.config.configurer.CsvLoadBuilder;
 import com.rejs.csvloader.config.configurer.JdbcBatchInsertRepositoryConfigurer;
+import com.rejs.csvloader.file.FileSystemAccessObject;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
@@ -17,5 +19,10 @@ public class CsvLoadConfiguration extends AbstractCsvLoadCommandLineRunnerConfig
                 .jdbcBatchInsertRepository((new JdbcBatchInsertRepositoryConfigurer()).withDefault())
                 .csvColumnValidateService((new CsvColumnValidateServiceConfigurer()).withDefault())
         ;
+    }
+
+    @Override
+    public FileSystemAccessObject fileSystemAccessObject() {
+        return new MultipartFileLocalSystemAccessObject();
     }
 }
