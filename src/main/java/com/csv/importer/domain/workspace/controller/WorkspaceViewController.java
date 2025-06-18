5f4f2f8912b10504aad037b62fedab7ce0ebb599
Lee jong sik <102013392+js990311@users.noreply.github.com>
2025-06-18 20:47:13 +0900

    feat(view): workspaces/crete

--- src/main/java/com/csv/importer/domain/workspace/controller/WorkspaceViewController.java
+++ src/main/java/com/csv/importer/domain/workspace/controller/WorkspaceViewController.java
@@ -1,15 +1,13 @@
 package com.csv.importer.domain.workspace.controller;
 
+import com.csv.importer.domain.workspace.controller.request.CreateWorkspaceRequest;
 import com.csv.importer.domain.workspace.dto.WorkspaceDto;
 import com.csv.importer.domain.workspace.service.WorkspaceService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequiredArgsConstructor
@@ -29,5 +27,17 @@ public class WorkspaceViewController {
         WorkspaceDto workspace = workspaceService.readById(workspaceId);
         model.addAttribute("workspace", workspace);
         return "/workspaces/id";
+    }
+
+    @GetMapping("/create")
+    public String getCreateWorkspace(Model model){
+        model.addAttribute("form", new CreateWorkspaceRequest());
+        return "workspaces/create";
+    }
+
+    @PostMapping("/create")
+    public String postCreteWorkspace(@ModelAttribute CreateWorkspaceRequest request){
+        WorkspaceDto workspace = workspaceService.createWorkspace(request.getName());
+        return "redirect:/workspaces/" + workspace.getId();
     }
 }
--- src/main/java/com/csv/importer/domain/workspace/controller/request/CreateWorkspaceRequest.java
+++ src/main/java/com/csv/importer/domain/workspace/controller/request/CreateWorkspaceRequest.java
@@ -2,9 +2,11 @@ package com.csv.importer.domain.workspace.controller.request;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @NoArgsConstructor
 @Getter
+@Setter
 public class CreateWorkspaceRequest {
     private String name;
 
